5245272bd9b6930cc6c1419f197c3508e6c10016
salvatore <totoilprincipe47@gmail.com>
2024-05-17 17:11:38 +0100

    article_category

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/',[PublicController::class , 'homepage'])->name('homepage');
 Route::get('/home',[PublicController::class , 'home'])->name('home');
 
 Route::get('/contattaci',[PublicController::class , 'contact'])->name('contattaci');
-Route::get('/contattaci-send', [PublicController::class, 'send'])->name('contattaci.create');
+Route::post('/contattaci-send', [PublicController::class, 'send'])->name('contattaci.create');
 
 Route::resource('articles', ArticleController::class);
 Route::resource('categories', CategoryController::class);
